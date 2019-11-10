438007ef1a54791eac466763dbb1ed517656d1a2
izamohammad <57467641+izamohammad@users.noreply.github.com>
2019-11-10 19:47:07 +0800

    Add files via upload

---
+++ LocationList.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace ExpressVPN2
+{
+    public class LocationList
+    {
+        public string Name { get; set; }
+        public int ip { get; set; }
+    }
+        
+    }
+
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace ExpressVPN2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void BindLocationList()
+        {
+            System.Net.ServicePointManager.ServerCertificateValidationCallback = delegate { return true; };
+
+            HttpClient client = new HttpClient
+            {
+                BaseAddress = new Uri("https://xvjune2014trial.apiary.io/")
+            };
+
+            client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/xml"));
+
+            HttpResponseMessage response = client.GetAsync("locations").Result;
+            if (response.IsSuccessStatusCode)
+            {
+                var location1 = response.Content.ReadAsAsync<IEnumerable<LocationList>>().Result;
+                grdLocation.ItemsSource = location1;
+            }
+            else
+            {
+                MessageBox.Show("Error: " + response.StatusCode);
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            BindLocationList();
+        }
+    }
+}
+
+                
+
+
+
